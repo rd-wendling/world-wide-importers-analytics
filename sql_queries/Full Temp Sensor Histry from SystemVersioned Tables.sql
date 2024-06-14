45d224b304ac1e6516806b59fae31e01e0091db1
RyanW <rwendling17@gmail.com>
2024-06-14 15:17:08 -0600

    wrote a couple example sql scripts, nothing added to dashboard

---
+++ sql_queries/Full Temp Sensor Histry from SystemVersioned Tables.sql
@@ -0,0 +1,92 @@
+
+/******************************************************************************************
+This is just to practice working with system version tables, in this case the script has 
+little functional purpose, so just hardcoding some arbitrary dates to work between here.
+*******************************************************************************************/
+declare @end_date   date = '2016-03-10'
+	,	@start_date date = '2015-12-30'
+	,	@cold_sensor int = 1
+	,	@min_coldRoomTempID int
+	,	@max_coldRoomTempID int
+	
+
+/******************************************************************************************
+Grab Max and Min Cold Room Temp ID based on Date Parameters
+
+	Getting these IDs so we can filter the select we're using later into our unioned
+	full history table. Doing it this way because the ColdRoomTemperatureID is an indexed
+	column so it will be faster to filter on this than in the ValidFrom/ValidTo columns.
+	This dataset is so small it doesn't make much functional difference, but I have run
+	into situations in the past where it certainly does. 
+
+	We first try to set these variables using only the current active table since it is
+	smaller and quicker. If it works the code will move on, if not it will try again on
+	the version history tables. Since between is inclusive we not only filter where a
+	date is between ValidFrom and ValidTo we also use min/max functions to get the desired
+	single interger ID outputs. 
+*******************************************************************************************/
+
+set @min_coldRoomTempID = (
+	select min(ct.ColdRoomTemperatureID)
+	from WideWorldImporters.Warehouse.ColdRoomTemperatures ct
+	where 1=1
+		and @start_date between ct.ValidFrom and ct.ValidTo
+		and ct.ColdRoomSensorNumber = @cold_sensor
+)
+set @max_coldRoomTempID = (
+	select max(ct.ColdRoomTemperatureID)
+	from WideWorldImporters.Warehouse.ColdRoomTemperatures ct
+	where 1=1
+		and @end_date between ct.ValidFrom and ct.ValidTo
+		and ct.ColdRoomSensorNumber = @cold_sensor
+)
+
+if @min_coldRoomTempID is null
+	set @min_coldRoomTempID = (
+		select min(cta.ColdRoomTemperatureID)
+		from WideWorldImporters.Warehouse.ColdRoomTemperatures_Archive cta
+		where 1=1
+			and @start_date between cta.ValidFrom and cta.ValidTo
+			and cta.ColdRoomSensorNumber = @cold_sensor
+	)
+
+if @max_coldRoomTempID is null
+	set @max_coldRoomTempID = (
+		select max(cta.ColdRoomTemperatureID)
+		from WideWorldImporters.Warehouse.ColdRoomTemperatures_Archive cta
+		where 1=1
+			and @end_date between cta.ValidFrom and cta.ValidTo
+			and cta.ColdRoomSensorNumber = @cold_sensor
+	)
+
+
+/******************************************************************************************
+Now get full history for the desired sensor into a temp table to work with
+
+There will not be any duplicate records due to the nature of the system version tables 
+(that simply would not be possible) so we can use union all to speed this up.
+*******************************************************************************************/
+drop table if exists #ColdRoomSensorOne
+
+	select *
+	into #ColdRoomSensorOne
+	from WideWorldImporters.Warehouse.ColdRoomTemperatures ct
+	where 1=1
+		and ct.ColdRoomSensorNumber = @cold_sensor
+
+union all
+
+	select *
+	from WideWorldImporters.Warehouse.ColdRoomTemperatures_Archive cta
+	where 1=1
+		and cta.ColdRoomSensorNumber = @cold_sensor
+
+
+/******************************************************************************************
+Now we have a single table with the entire history of this particular cold sensors 
+readings to work off of.
+*******************************************************************************************/
+select *
+from #ColdRoomSensorOne c
+order by
+	c.ValidFrom desc
---
+++ sql_queries/MetaData -- Find All Tables with Column Name like X.sql
@@ -0,0 +1,124 @@
+
+/********************************************************************************************
+Declare loop start, using 5 to skip system databases like master, tempdb, etc
+*********************************************************************************************/
+declare @i int = 5
+
+
+/********************************************************************************************
+Set up tables for results and errors
+
+ -- must use a global temp for results given passing statement to exec sp_executesql
+*********************************************************************************************/
+if object_id('tempdb..##results') is not null
+	drop table ##results;
+
+create table ##results (
+							 [ColumnName]				varchar(max)
+						 ,	 [TableName]				varchar(max)
+						 ,	 [DatabaseName]				varchar(max)
+						 ,	 [DType]					varchar(max)
+						 ,	 [MaxLength]				bigint
+						 ,	 [ColumnIndexed?]			int
+						 ,	 [IndexType]				varchar(12)
+						 ,	 [IndexName]				varchar(max)
+					   )
+
+if object_id('tempdb..#errors') is not null
+	drop table #errors;
+
+create table #errors (
+							 [Database_ErrorRaised] varchar(max)
+						 ,	 [ErrorNumber] 			int
+						 ,	 [ErrorSeverity] 		int
+						 ,	 [ErrorState]  			varchar(max)
+						 ,	 [ErrorLine]  			int
+						 ,	 [ErrorMessage]			varchar(max)
+					  )
+		
+
+
+/********************************************************************************************
+Loop over each database in server
+
+Can make changes to WHERE clause (starting line 89) if looking for something specific
+*********************************************************************************************/	
+
+while @i <= (select count(d.database_id) from sys.databases d)
+
+begin 
+	begin try
+		declare @database varchar(max)
+		set @database = (select concat('[', d.name, ']') from sys.databases d where d.database_id = @i)
+
+		declare @strSQL nvarchar(max)
+		set @strSQL = '
+
+		use '+@database+';
+
+		insert into ##results
+
+		select 
+
+				c.name														as [ColumnName]
+			,	t.name														as [TableName]
+			,	'+concat(char(39),@database,char(39))+'						as [DatabaseName]
+			,	ty.name														as [DType]
+			,	ty.max_length												as [MaxLength]
+			,	case when ic.column_id is null then 0 else 1 end			as [ColumnIndexed?]
+			,	i.type_desc													as [IndexType]
+			,	i.name														as [IndexName]
+
+		from sys.columns c
+
+		join sys.tables  t   
+			on c.object_id = t.object_id
+
+		join sys.types ty
+			on ty.user_type_id = c.user_type_id
+
+		left join sys.index_columns ic
+			on ic.object_id = t.object_id
+			and ic.column_id = c.column_id
+
+		left join sys.indexes i
+			on ic.object_id = i.object_id
+			and ic.index_id = i.index_id
+
+		where 1=1
+			and c.name like ''%InventoryLegID%''								-- Uncomment to help find tables/cols with column names containing input string(s)
+		--	and ic.column_id is not null							-- Uncomment to only return columns that are indexed
+		'
+		
+		exec sp_executesql @strSQL
+
+		set @i = @i + 1
+
+	end try
+	begin catch
+
+		insert into #errors
+		select  
+
+				@database			as [Database_ErrorRaised]
+			,	error_number()		as [ErrorNumber] 
+			,	error_severity()	as [ErrorSeverity]  
+			,	error_state()		as [ErrorState]  
+			,	error_line()		as [ErrorLine]  
+			,	error_message()		as [ErrorMessage]
+		
+		set @i = @i + 1
+
+	end catch
+		
+end
+
+
+
+/********************************************************************************************
+Show results and any errors encountered in the while loop
+*********************************************************************************************/	
+select * from ##results
+select * from #errors
+
+
